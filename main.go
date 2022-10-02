6171bead49a6ea2caa8445b74cf8c2f550952525
0xpeIpeI <hokumaru12@gmail.com>
2022-10-02 20:45:15 +0900

    linebot clientをcontextで共有

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("環境変数ファイルを読み込めませんでした")
+		return
+	}
+
+	http.HandleFunc("/callback", handler)
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -21,14 +29,65 @@ func main() {
 	// Start HTTP server.
 	log.Printf("listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	name := os.Getenv("NAME")
 	if name == "" {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func LINEWebhookHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func LINEClientMiddleware(h http.Handler) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			bot, err := linebot.New(
+				os.Getenv("CHANNEL_SECRET"),
+				os.Getenv("CHANNEL_TOKEN"),
+			)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			// clientをハンドラー間で共有するためにコンテキストに登録
+			SetLINEClientCtx(r.Context(), bot)
+			events, err := bot.ParseRequest(r)
+			if err != nil {
+				if err == linebot.ErrInvalidSignature {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				} else {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+			}
+			// イベントもハンドラー間で共有するためにコンテキストに登録。※イベントは正直コンテキストに入れず、橋渡ししてもいいかも知らない
+			SetLINEEventsCtx(r.Context(), events)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func SetLINEClientCtx(ctx context.Context, bot *linebot.Client) context.Context {
+	return context.WithValue(ctx, linebot.Client{}, bot)
+}
+
+func SetLINEEventsCtx(ctx context.Context, events []*linebot.Event) context.Context {
+	return context.WithValue(ctx, []linebot.Event{}, events)
+}
+
+func GetLINEEvents(ctx context.Context) ([]linebot.Event, bool) {
+	events, err := ctx.Value([]linebot.Event{}).([]linebot.Event)
+	return events, err
 }
